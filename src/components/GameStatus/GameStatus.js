320c06bed83153e5ebf67e43f55f4239280d31e2
riya09 <faria.ayir@gmail.com>
2023-02-11 12:05:41 +0600

    feat:added component for showing banner

---
+++ src/components/GameStatus/GameStatus.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function GameStatus({status, answer, numberOfGuesses}) {
+  const banner = () => {
+    if (status === 'win') {
+      return (
+        <p>
+          <strong>Congratulations!</strong> Got it in
+          <strong>{numberOfGuesses} guesses</strong>.
+        </p>
+      )
+    } else {
+      return (
+        <p>Sorry, the correct answer is <strong>{answer}</strong>.</p>
+      )
+    }
+  }
+  
+  return (
+    <>
+      <div className={status === 'win' ? 'happy banner' : 'sad banner'}>
+        {banner}
+      </div>
+    </>
+  );
+}
+
+export default GameStatus;
