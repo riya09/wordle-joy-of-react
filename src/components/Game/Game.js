c41dc57bf71fc5b55cd71a9a23da3c164a9151ce
riya09 <faria.ayir@gmail.com>
2023-02-11 11:48:25 +0600

    feat:added logic to check status of the game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,7 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput/GuessInput';
 import GuessList from '../GuessList/GuessList';
+import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -12,11 +13,23 @@ console.info({ answer });
 
 function Game() {
   const [guessedWordList, setGuessedWordList] = React.useState([])
+  const [gameStatus, setGameStatus] = React.useState('')
+  const checkAnswer = (word) => {
+    setGuessedWordList([...guessedWordList, word])
+    if (word === answer) {
+      setGameStatus('win')
+    } else if(guessedWordList.length === NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus('lose')
+    }
+  }
   return (
   <>
     <GuessList guessedWords={guessedWordList} answer={answer}/>
-    <GuessInput getWord={(word) => setGuessedWordList([...guessedWordList, word])}/>
-  </>);
+    <GuessInput
+      disabled={!!gameStatus}
+      getWord={(word) => checkAnswer(word)}/>
+  </>
+  );
 }
 
 export default Game;
