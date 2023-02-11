d53bcdbb4c3109d5d4d82626254fb113da5623ef
riya09 <faria.ayir@gmail.com>
2023-02-11 13:01:24 +0600

    chore: removed console

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -9,8 +9,6 @@ import GameStatus from '../GameStatus/GameStatus';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
-// To make debugging easier, we'll log the solution in the console.
-console.info({ answer });
 
 function Game() {
   const [guessedWordList, setGuessedWordList] = React.useState([])
