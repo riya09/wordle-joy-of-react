5e7ca027ee70c2491b17efa099533aeba079501d
riya09 <faria.ayir@gmail.com>
2023-02-11 13:00:57 +0600

    feat: imported banner status in game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -5,6 +5,7 @@ import { WORDS } from '../../data';
 import GuessInput from '../GuessInput/GuessInput';
 import GuessList from '../GuessList/GuessList';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
+import GameStatus from '../GameStatus/GameStatus';
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS);
@@ -15,10 +16,11 @@ function Game() {
   const [guessedWordList, setGuessedWordList] = React.useState([])
   const [gameStatus, setGameStatus] = React.useState('')
   const checkAnswer = (word) => {
-    setGuessedWordList([...guessedWordList, word])
+    const updatedGuessList = [...guessedWordList, word]
+    setGuessedWordList(updatedGuessList)
     if (word === answer) {
       setGameStatus('win')
-    } else if(guessedWordList.length === NUM_OF_GUESSES_ALLOWED) {
+    } else if(updatedGuessList.length === NUM_OF_GUESSES_ALLOWED) {
       setGameStatus('lose')
     }
   }
@@ -28,6 +30,13 @@ function Game() {
     <GuessInput
       disabled={!!gameStatus}
       getWord={(word) => checkAnswer(word)}/>
+
+    {gameStatus
+      && <GameStatus
+            status={gameStatus}
+            numberOfGuesses={guessedWordList.length}
+            answer={answer} />
+    }
   </>
   );
 }
